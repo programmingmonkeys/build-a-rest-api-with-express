4eee3531a2b6bc7cc165d68c3f063be3a3e42378
codingChewie <matthewtimothy910@yahoo.com>
2019-11-08 14:07:11 -0600

    Building API

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3000
+const mongoose = require('mongoose')
+const logger = require('morgan')
+const routes = require('./routes')
 
-const jsonCheck = (req, res, next) => {
-  if (req.body) {
-    console.log(`the sky is, ${req.body.color}`)
-  } else {
-    console.log(null)
-  }
-  next()
-}
-
-app.use(jsonCheck)
-
+app.use(logger('dev'))
 app.use(express.json())
 
-app.use(jsonCheck)
+mongoose.connect('mongodb://localhost:27017/qa', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
 
-app.listen(PORT, console.log(`express listening on port: ${PORT}`))
+db.on('error', (err) => console.error('connection error:', err))
+
+db.once('open', () => console.log('db connection successful'))
+
+app.use('/questions', routes)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+// Error Handler
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500)
+  res.json({
+    error: {
+      message: err.message,
+    },
+  })
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Express server is listening on port: ${PORT}`))
