80a8f05194afc2480cb479f9d018e3f115e87084
codingChewie <matthewtimothy910@yahoo.com>
2019-11-08 13:09:27 -0600

    Accepting Data

--- app.js
+++ app.js
@@ -2,4 +2,19 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 
+const jsonCheck = (req, res, next) => {
+  if (req.body) {
+    console.log(`the sky is, ${req.body.color}`)
+  } else {
+    console.log(null)
+  }
+  next()
+}
+
+app.use(jsonCheck)
+
+app.use(express.json())
+
+app.use(jsonCheck)
+
 app.listen(PORT, console.log(`express listening on port: ${PORT}`))
